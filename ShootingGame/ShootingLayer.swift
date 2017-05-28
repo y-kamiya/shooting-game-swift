b341580c119008e0c10bbeb4c8c790e631d6257e
Yuji Kamiya <y.kamiya0@gmail.com>
2017-05-28 16:41:19 +0900

    shooting layer

---
+++ ShootingGame/ShootingLayer.swift
@@ -0,0 +1,77 @@
+//
+//  ShootingLayer.swift
+//  ShootingGame
+//
+//  Created by Yuji Kamiya on 2017/05/28.
+//  Copyright © 2017年 uj. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class ShootingLayer: SKNode, SKPhysicsContactDelegate {
+    
+    enum NodeName: String {
+        case player = "player"
+        case wall = "wall"
+    }
+    
+    let player = Player()
+    
+    convenience init(with size: CGSize) {
+        self.init()
+        self.initialize(size)
+    }
+    
+    func initialize(_ size: CGSize) {
+        let frameWidth = size.width
+        let frameHeight = size.height
+        
+        player.position = CGPoint(x: frameWidth / 2, y: 50)
+        print(player.position)
+        player.name = NodeName.player.rawValue
+        self.addChild(player)
+        
+        let origin = CGPoint(x:-50, y:-50)
+        let size = CGSize(width: frameWidth + 100, height: frameHeight + 100)
+        let wall = SKShapeNode(rect: CGRect(origin: origin, size: size))
+        wall.physicsBody = SKPhysicsBody(edgeChainFrom: wall.path!)
+        wall.physicsBody?.categoryBitMask = ContactCategory.wall
+        wall.physicsBody?.collisionBitMask = ContactCategory.none
+        wall.physicsBody?.contactTestBitMask = ContactCategory.all
+        wall.name = NodeName.wall.rawValue
+        self.addChild(wall)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.shot), name: Notification.Name("shot"), object: nil)
+        
+    }
+    
+    @objc private func shot() {
+        guard let player = childNode(withName: NodeName.player.rawValue) else {
+            return;
+        }
+        let shot = Bullet(position: player.position)
+        self.addChild(shot)
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if (contact.bodyA.categoryBitMask != ContactCategory.wall) {
+            contact.bodyA.node?.removeFromParent();
+        }
+        if (contact.bodyB.categoryBitMask != ContactCategory.wall) {
+            contact.bodyB.node?.removeFromParent();
+        }
+    }
+    
+    func movePlayer(direction: CGPoint) {
+        guard let player = childNode(withName: NodeName.player.rawValue) as? Player else {
+            return;
+        }
+        player.move(for: direction)
+    }
+    
+    func createEnemy() {
+        let enemy = Enemy(frame: self.scene!.view!.frame)
+        self.addChild(enemy)
+    }
+}
--- ShootingGame/TestScene.swift
+++ ShootingGame/TestScene.swift
@@ -9,41 +9,19 @@
 import Foundation
 import SpriteKit
 
-class TestScene:SKScene, SKPhysicsContactDelegate {
-    
-    enum NodeName: String {
-        case player = "player"
-        case wall = "wall"
-    }
+class TestScene:SKScene {
     
     let playerController = PlayerController()
+    var shootingLayer: ShootingLayer?
     
     override func didMove(to view: SKView) {
-        
-        self.physicsWorld.contactDelegate = self
-        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
-        
         self.addChild(playerController)
         
-        let frameWidth = view.frame.width 
-        let frameHeight = view.frame.height
+        shootingLayer = ShootingLayer(with: view.frame.size)
+        self.addChild(shootingLayer!)
         
-        let player = Player()
-        player.position = CGPoint(x: frameWidth / 2, y: 50)
-        player.name = NodeName.player.rawValue
-        self.addChild(player)
-        
-        let origin = CGPoint(x:-50, y:-50)
-        let size = CGSize(width: frameWidth + 100, height: frameHeight + 100)
-        let wall = SKShapeNode(rect: CGRect(origin: origin, size: size))
-        wall.physicsBody = SKPhysicsBody(edgeChainFrom: wall.path!)
-        wall.physicsBody?.categoryBitMask = ContactCategory.wall
-        wall.physicsBody?.collisionBitMask = ContactCategory.none
-        wall.physicsBody?.contactTestBitMask = ContactCategory.all
-        self.addChild(wall)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(self.shot), name: Notification.Name("shot"), object: nil)
-        
+        self.physicsWorld.contactDelegate = shootingLayer
+        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -58,23 +36,6 @@ class TestScene:SKScene, SKPhysicsContactDelegate {
         playerController.touchesEnded(touches, with: event)
     }
     
-    @objc private func shot() {
-        guard let player = childNode(withName: NodeName.player.rawValue) else {
-            return;
-        }
-        let shot = Bullet(position: player.position)
-        self.addChild(shot)
-    }
-    
-    func didBegin(_ contact: SKPhysicsContact) {
-        if (contact.bodyA.categoryBitMask != ContactCategory.wall) {
-            contact.bodyA.node?.removeFromParent();
-        }
-        if (contact.bodyB.categoryBitMask != ContactCategory.wall) {
-            contact.bodyB.node?.removeFromParent();
-        }
-    }
-    
     private var elapsedSeconds: TimeInterval = 0
     private var lastUpdatedTime: TimeInterval = 0
     
@@ -84,8 +45,7 @@ class TestScene:SKScene, SKPhysicsContactDelegate {
         elapsedSeconds += currentTime - lastUpdatedTime
         if (ENEMY_CREATED_INTERVAL < elapsedSeconds) {
             elapsedSeconds = 0
-            let enemy = Enemy(frame: (view?.frame)!)
-            self.addChild(enemy)
+            shootingLayer?.createEnemy()
         }
         lastUpdatedTime = currentTime
         
@@ -95,12 +55,8 @@ class TestScene:SKScene, SKPhysicsContactDelegate {
         guard let movedPos = playerController.getTouchMovedPosition() else {
             return;
         }
-        
-        guard let player = childNode(withName: NodeName.player.rawValue) as? Player else {
-            return;
-        }
         let direction = self.getUnitVecor(start: beganPos, end: movedPos)
-        player.move(for: direction)
+        shootingLayer?.movePlayer(direction: direction)
     }
     
     private func getUnitVecor(start: CGPoint, end: CGPoint) -> CGPoint {
